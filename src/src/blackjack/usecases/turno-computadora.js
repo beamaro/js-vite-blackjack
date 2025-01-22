2500e6662a24a39842ebef50359763047bddf5f6
Beatriz Amaro <beamaro.19@gmail.com>
2025-01-21 22:49:22 -0300

    First commit

---
+++ src/src/blackjack/usecases/turno-computadora.js
@@ -0,0 +1,55 @@
+
+
+import { pedirCarta, valorCarta, crearCartaHTML } from "./";
+
+
+/**
+ * 
+ * @param {Number} puntosMinimos que la compu necesita para ganar
+ * @param {HTMLElement} puntosHTML HTML para mostrar los puntos
+ * @param {HTMLElement} divCartasComputadora HTML para mostrar las cartas
+ * @param {Array<String>} deck 
+ */
+
+// turno de la computadora
+export const turnoComputadora = ( puntosMinimos, puntosHTML, divCartasComputadora, deck = [] ) => {
+
+
+    if (!puntosMinimos) throw new Error('Puntos minimos son necesarios');
+    if (!puntosHTML) throw new Error('Argumento puntosHtml es necesario');
+
+
+    let puntosComputadora = 0;
+
+
+    do {
+        const carta = pedirCarta(deck);
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHTML.innerText = puntosComputadora;
+
+        
+        const imgCarta = crearCartaHTML(carta);
+
+        
+
+        divCartasComputadora.append( imgCarta );
+
+        if( puntosMinimos > 21 ) {
+            break;
+        }
+
+    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+    setTimeout(() => {
+        if( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana')
+        } else if( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100 );
+}
